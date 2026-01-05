699b90baf124bd2bb62d86adfc47fa8f34cbd2ca
KYS <go030515@gmail.com>
2026-01-05 11:25:52 +0900

    navibar

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -2,6 +2,10 @@ import { useState } from "react";
 import "../styles/Navbar.css";
 import logoImg from "../images/logo.png";
 import { useNavigate } from "react-router-dom";
+import homeicon from "../icons/home.svg";
+import communityicon from "../icons/community.svg";
+import calendarincon from "../icons/calendar.svg";
+import myicon from "../icons/my.svg";
 
 export default function Navbar() {
   const nav = useNavigate();
@@ -19,10 +23,18 @@ export default function Navbar() {
             </div>
 
             <ul className="nav-links">
-            <li><button onClick={() => nav("/")} className="nav-btn">홈</button></li>
-            <li><button onClick={() => nav("/recommend")} className="nav-btn">커뮤니티</button></li>
-            <li><button onClick={() => nav("/team")} className="nav-btn">캘린더</button></li>
-            <li><button onClick={() => nav("/contact")} className="nav-btn">MY팀</button></li>
+            <li><button onClick={() => nav("/")} className="nav-btn">
+              <img src={homeicon} alt="homeicon"
+              style={{width:"25px", height:"25px", textAlign:"center"}} />홈</button></li>
+            <li><button onClick={() => nav("/community")} className="nav-btn">
+              <img src={communityicon} alt="communityicon"
+              style={{width:"25px", height:"25px", textAlign:"center"}} />커뮤니티</button></li>
+            <li><button onClick={() => nav("/team")} className="nav-btn">
+              <img src={calendarincon} alt="calendaricon"
+              style={{width:"25px", height:"25px", textAlign:"center"}} />캘린더</button></li>
+            <li><button onClick={() => nav("/contact")} className="nav-btn">
+              <img src={myicon} alt="myicon"
+              style={{width:"30px", height:"30px", textAlign:"center"}} />MY팀</button></li>
             </ul>
         </div>
     </nav>
